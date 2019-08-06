8ac9529893d7df13c300a23035a8d9bc659ad50c
chae224 <chaegoru@gmail.com>
2019-08-06 10:08:36 +0200

    slider store base

---
+++ js/bloodydemocracy.js
@@ -0,0 +1,30 @@
+///////////////////////////Sylvain//////////////////////////////////////////
+
+var storeSlider1 = tns({
+	container: ".argent",
+	item: 1,
+	controls: false,
+	navContainer: "#customize-thumbnails1",
+	touch: true,
+	mouseDrag: true,
+});
+
+var storeSlider2 = tns({
+	container: ".vote",
+	item: 1,
+	controls: false,
+	navContainer: "#customize-thumbnails2",
+	touch: true,
+	mouseDrag: true,
+});
+
+var storeSlider3 = tns({
+	container: ".gestion",
+	item: 1,
+	controls: false,
+	navContainer: "#customize-thumbnails3",
+	touch: true,
+	mouseDrag: true,
+});
+
+///////////////////////////FinSylvain///////////////////////////////////////
