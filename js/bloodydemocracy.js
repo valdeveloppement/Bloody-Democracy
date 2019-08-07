69ed99fc29d27bdcad39da1a856870de68a219c5
jdelak <delakdev@gmail.com>
2019-08-07 11:20:54 +0200

    ajout des compteurs + gestion du click (js) + css inclus + sons + merge du js avec celui de Valentin

---
+++ js/bloodydemocracy.js
@@ -0,0 +1,280 @@
+
+
+
+
+/////////////////////////////////////// VALENTIN  /////////////////////////////////////////////
+
+$clicker = document.getElementById('clicker');
+$brasDessous = document.getElementById('brasDessous');
+$brasDessus = document.getElementById('brasDessus');
+$brasPaper = document.getElementById('brasPaper');
+$nbClicks=0;
+/////////////////////// JULIEN ////////////////////////////////////
+$containerCompteurVote = document.getElementById('vote');
+$containerCompteurArgent = document.getElementById('argent');
+
+// S'il n'y a pas de cookies compteur vote, le compteur est à 0 sinon on récupère le nombre de vote stocké
+if (localStorage.getItem("compteurVote") === null) {
+  $compteurVote =  0;
+}else{
+  $compteurVote = parseInt(localStorage.getItem("compteurVote"));
+}
+// S'il n'y a pas de cookies compteur argent, le compteur est à 0 sinon on récupère le nombre d'argent stocké
+if (localStorage.getItem("compteurArgent") === null) {
+  $compteurArgent =  0;
+}else{
+  $compteurArgent = parseInt(localStorage.getItem("compteurArgent"));
+}
+
+$containerCompteurVote.innerHTML = $compteurVote;
+$containerCompteurArgent.innerHTML = $compteurArgent;
+$compteurVotesTotal = 0;
+$compteurArgentTotal = 0;
+///////////////////////// FIN JULIEN //////////////////////////////////////////////////
+
+
+// Cette fonction met à jour les positions quand on change la taille d'ecran.
+function placeClicker() {
+
+  $hauteurTable=document.getElementById("table").offsetHeight;
+  $yPos= 0.75*$hauteurTable;
+
+  $clicker.style.position = "absolute";
+  $clicker.style.bottom = $yPos+'px';
+
+
+
+  $yBrasPos= 0.6*$hauteurTable
+
+  $brasPaper.style.bottom = $yBrasPos+'px';
+  $brasDessus.style.bottom = $yBrasPos+'px';
+  $brasDessous.style.bottom = $yBrasPos+'px';
+
+
+
+}
+
+setInterval( placeClicker, 15);
+
+
+
+
+// Fonctions qui animent le click
+var $boiteDessus=document.getElementById("boiteDessus");
+var $boiteDessous=document.getElementById("boiteDessous");
+
+$boiteDessus.onclick=click;
+$boiteDessus.onmouseenter=playBloup;
+$boiteDessus.onmouseleave=playBloup;
+
+
+
+$angle=0;
+
+function downBras(){
+
+  $angle=$angle+1;
+
+  if ($angle<10){
+    $brasDessus.style.transform = "rotate("+$angle+"deg)";
+    $brasPaper.style.transform = "rotate("+$angle+"deg)";
+    $brasDessous.style.transform = "rotate("+$angle+"deg)";
+  }
+
+  else{
+    clearInterval($vitesseDown);
+    $vitesseDownPaper =setInterval( downPaper, 7);
+    $vitesseUp = setInterval( upBras, 6);
+    
+
+  }
+
+}
+
+$chute=0;
+
+function downPaper(){
+
+  $brasDessusPosition = $brasDessus.offsetTop;
+  $hauteurBoiteDessus=$boiteDessus.offsetHeight;
+  $chute=$chute+2;
+
+
+  if($chute < 0.3*$hauteurBoiteDessus){
+    $brasPaper.style.top = $brasDessusPosition+$chute+'px';
+  }
+
+
+  else{
+
+
+ 
+    $brasPaper.style.transform = "rotate(-11deg)";
+    $brasPaper.style.top = $brasDessusPosition+'px';
+    
+    clearInterval($vitesseDownPaper);
+
+
+  }
+
+}
+
+function upBras(){
+
+  $angle=$angle-1;
+
+  if ($angle>-12){
+    $brasDessus.style.transform = "rotate("+$angle+"deg)";
+    $brasDessous.style.transform = "rotate("+$angle+"deg)";
+  }
+
+  else{
+
+
+    clearInterval($vitesseUp);
+
+  }
+
+}
+
+
+// Fonction du click sur la boite
+
+function click() {
+
+  if($nbClicks>0){
+    $chute=0;
+    $angle=0;    
+    clearInterval($vitesseUp);
+    clearInterval($vitesseDownPaper);
+    clearInterval($vitesseDown);
+
+  }
+  
+  playBloup();
+
+  $vitesseDown = setInterval( downBras, 6);
+
+  $nbClicks=$nbClicks+1;
+  console.log($nbClicks)
+  ///////////////////////////////////// PARTIE JULIEN ////////////////////
+  $compteurVote++;
+  $compteurArgent+=5;
+  $compteurVotesTotal++;
+  $compteurArgentTotal+= 5;
+  $containerCompteurVote.innerHTML = $compteurVote;
+  $containerCompteurArgent.innerHTML = $compteurArgent;
+  $audio = new Audio('sons/vote.mp3');
+  $audio.play();
+  localStorage.setItem('compteurVote', $compteurVote);
+  localStorage.setItem('compteurArgent', $compteurArgent);
+  localStorage.setItem('compteurVotesTotal', $compteurVotesTotal);
+  localStorage.setItem('compteurArgentTotal', $compteurArgentTotal);
+  ////////////////////////////////////// FIN JULIEN /////////////////////
+
+}
+
+
+
+// Fonctions qui animent l'effet "bloop"
+
+$zoom=0;
+$entropie=1;
+function bloup(){
+$zoom=$zoom+1;
+if (($zoom<10/$entropie) & $entropie<=2){
+$boiteDessus.style.transform="scale(1.0"+$zoom+")";
+$boiteDessous.style.transform="scale(1.0"+$zoom+")";
+console.log("test")
+}
+else{
+  clearInterval($bloup)
+  $bloupReverse= setInterval( bloupReverse, 10);
+}
+
+}
+
+
+function bloupReverse(){
+  $zoom=$zoom-1;
+  if ($zoom>=0){
+  $boiteDessus.style.transform="scale(1.0"+$zoom+")";
+  $boiteDessous.style.transform="scale(1.0"+$zoom+")";
+  console.log("test")
+  }
+  else{
+    clearInterval($bloupReverse);
+    $entropie=$entropie+1;
+
+    $bloup= setInterval( bloup, 10);
+  }
+  
+}
+
+
+
+  
+ $nbMouseEnter=0;
+function playBloup(){
+  $nbMouseEnter=$nbMouseEnter+1;
+
+  if($nbMouseEnter>1){
+    $zoom=0;
+    $entropie=1;
+    clearInterval($bloup)
+    clearInterval($bloupReverse);
+  }
+
+
+
+
+  $bloup= setInterval( bloup, 10);
+
+
+}
+
+
+
+
+// Sinus qui s'attenue :  ((6+cos(x-3)-0.5(x-3))/6)^2
+
+
+
+
+/////////////////////////////////////// FIN VALENTIN  /////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+od = new Odometer({
+  el: $containerCompteurVote,
+  value: $compteurVote,
+
+  // Any option (other than auto and selector) can be passed in here
+  format: 'd',
+  theme: 'train-station',
+  duration:200,
+  //animation: 'count'
+});
+
+
+
+od1 = new Odometer({
+  el: $containerCompteurArgent,
+  value: $compteurArgent,
+
+  // Any option (other than auto and selector) can be passed in here
+  format: 'd',
+  theme: 'dollar',
+  duration:200,
+  //animation: 'count'
+});
+
+
+//////////////////////////////// FIN JULIEN ///////////////////////
